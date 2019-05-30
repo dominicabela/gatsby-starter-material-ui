3a630ff9c633109083e4ec7b768f3cb6ca99238d
Dominic Abela <d.j.m.abela@gmail.com>
2019-05-29 23:38:24 -0400

    edit siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Material UI Starter`,
+    description: `Kick off your next, great Gatsby project with this Material UI starter. This barebones starter ships with the main Gatsby and Material UI configuration files you might need.`,
+    author: `@dominicabela`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
